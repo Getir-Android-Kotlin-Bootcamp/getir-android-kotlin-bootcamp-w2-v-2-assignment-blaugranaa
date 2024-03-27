8c1cdd7d2083cd0e68ebc3fc6f657d279f4cc517
Ayşenur Yavuz <aysenur.fcb@gmail.com>
2024-03-27 03:15:41 +0300

    google maps places implemented

--- app/src/main/java/com/getir/patika/foodcouriers/MainActivity.kt
+++ app/src/main/java/com/getir/patika/foodcouriers/MainActivity.kt
@@ -3,6 +3,9 @@ package com.getir.patika.foodcouriers
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.viewpager2.widget.ViewPager2
+import com.google.android.libraries.places.api.Places
+import com.google.android.libraries.places.api.model.Place
+import com.google.android.libraries.places.widget.AutocompleteSupportFragment
 import com.google.android.material.tabs.TabLayout
 import com.google.android.material.tabs.TabLayoutMediator
 
@@ -35,5 +38,17 @@ class MainActivity : AppCompatActivity() {
 
         }.attach() */
 
+        if(!Places.isInitialized()){
+            Places.initialize(applicationContext, "AIzaSyBEkN8FBe4Ji7iWQgJ4WDyTocgVVe0XgL8")
+            val autocompleteSupportFragment = (supportFragmentManager.findFragmentById(R.id.place_autocomplete) as AutocompleteSupportFragment).setPlaceFields(
+                listOf(Place.Field.LAT_LNG, Place.Field.NAME)
+            )
+
+          //  autocompleteSupportFragment.setOnPlaceSelectedListener()
+
+        }
+
     }
+
+
 }
