77c7af22f2f3f4142dd11127d17e2105106be0ed
Ayşenur Yavuz <aysenur.fcb@gmail.com>
2024-03-28 23:10:34 +0300

    fragment call fixed

--- app/src/main/java/com/getir/patika/foodcouriers/MainActivity.kt
+++ app/src/main/java/com/getir/patika/foodcouriers/MainActivity.kt
@@ -11,44 +11,13 @@ import com.google.android.material.tabs.TabLayoutMediator
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var tabLayout: TabLayout
-    private lateinit var viewPager2: ViewPager2
-    private lateinit var pagerAdapter: PagerAdapter
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.fragment_maps)
-      /*  tabLayout = findViewById(R.id.tab_account)
-        viewPager2 = findViewById(R.id.viewpager_account)
-        pagerAdapter = PagerAdapter(supportFragmentManager,lifecycle).apply {
-            addFragment(CreateAccountFragment())
-            addFragment(LoginAccountFragment())
-        }
-        viewPager2.adapter = pagerAdapter
 
-        TabLayoutMediator(tabLayout,viewPager2){ tab, position ->
-             when(position) {
-                 0 -> {
-                     tab.text = "Create Account"
-                 }
-                 1 -> {
-                     tab.text = "Login Account"
-                 }
-             }
+        val fragment = MapsFragment()
 
-        }.attach() */
-
-        if(!Places.isInitialized()){
-            Places.initialize(applicationContext, "AIzaSyBEkN8FBe4Ji7iWQgJ4WDyTocgVVe0XgL8")
-            val autocompleteSupportFragment = (supportFragmentManager.findFragmentById(R.id.place_autocomplete) as AutocompleteSupportFragment).setPlaceFields(
-                listOf(Place.Field.LAT_LNG, Place.Field.NAME)
-            )
-
-          //  autocompleteSupportFragment.setOnPlaceSelectedListener()
-
-        }
-
+        // Fragmentı açma
+        supportFragmentManager.beginTransaction().replace(R.id.map, fragment).commit()
     }
-
-
 }
